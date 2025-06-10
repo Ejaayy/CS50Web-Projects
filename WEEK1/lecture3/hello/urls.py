4a47ce4e052f1bf911a2bec65c3413edd0db0564
Edriene Paingan <painganedrienejames@gmail.com>
2025-06-10 10:22:09 +0800

    added an index.html

--- WEEK1/lecture3/hello/urls.py
+++ WEEK1/lecture3/hello/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("ej", views.ej, name = "ej"), #opens the function inside views like methods
     path("rain", views.rain, name = "rain")
+    
 ]
--- WEEK1/lecture3/hello/views.py
+++ WEEK1/lecture3/hello/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def ej(request):
     return HttpResponse("Hello EJ!")
 
 def rain(request):
     return HttpResponse("Hello Rain!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
